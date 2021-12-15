f7954000fe8df92a5b964658bc1ac6ab9731b37d
hstrychalski <hubert.strychalski@centra.com>
2021-12-15 16:46:50 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { ApolloServer } from 'apollo-server-fastify';
+import { ApolloServer, gql } from 'apollo-server-fastify';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import fastify, { FastifyInstance } from 'fastify';
 
-function fastifyAppClosePlugin(app: FastifyInstance): ApolloServerPlugin {
+function fastifyAppClosePlugin(app: FastifyInstance) {
   return {
     async serverWillStart() {
       return {
@@ -30,3 +30,24 @@ async function startApolloServer(typeDefs, resolvers) {
   await app.listen(4000);
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+
+
+startApolloServer(typeDefs,[])
